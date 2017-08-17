700adf3d2c91416841b67c4147d05c14ef88ab9c
YaboWei <weiyabobeijing@gmail.com>
2017-08-17 23:34:55 +0800

    everse Integer

--- 7.ReverseInteger.c
+++ 7.ReverseInteger.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+
 int reverse(int x) {
-    
+    int rev = 0;
+    int sig = x > 0 ? 1 : -1;;
+    if (x < 0) x = -x;
+    while (x % 10 == 0) {
+        x /= 10;
+    }
+
+    while (x) {
+        rev = rev*10 + x % 10;
+        x /= 10;
+    }
+
+    return rev*sig;
+}
+
+int main()
+{
+    printf("reverse(%d): %d\n", 123, reverse(123));
+    printf("reverse(%d): %d\n", -123, reverse(-123));
+    printf("reverse(%d): %d\n", 100, reverse(100));
+    printf("reverse(%d): %d\n", -100, reverse(-100));
+    printf("reverse(%d): %d\n", 1000000003 , reverse(1000000003 ));
 }
