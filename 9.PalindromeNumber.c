514de8762c18e1be2239324c6899af36e4c98361
YaboWei <weiyabobeijing@gmail.com>
2017-08-18 15:56:27 +0800

    Palindrome Number

--- 9.PalindromeNumber.c
+++ 9.PalindromeNumber.c
@@ -1,3 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 bool isPalindrome(int x) {
-    
+    int rev = 0;
+
+    while (x > rev) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+
+    return x == rev || x == rev/10;
+}
+
+int main()
+{
+    int val = 121;
+    printf("%9d %s\n", val, isPalindrome(val) ? "Palindrome" : "no");
+    val = 12321;
+    printf("%9d %s\n", val, isPalindrome(val) ? "Palindrome" : "no");
+    val = 123321;
+    printf("%9d %s\n", val, isPalindrome(val) ? "Palindrome" : "no");
 }
