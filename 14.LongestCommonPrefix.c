7999d0c0f2f35613559a99776340fd40a8f62e2f
YaboWei <weiyabobeijing@gmail.com>
2017-08-19 23:43:06 +0800

    Longest Common Prefix

--- 14.LongestCommonPrefix.c
+++ 14.LongestCommonPrefix.c
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char* longestCommonPrefix(char** strs, int strsSize) {
-    
+    int i = 0;
+    char* comPre = malloc(100);
+    char* first_str = strs[0];
+    int minLen = 100;
+    for (i = 0; i < strsSize; i++) {
+        int len = strlen(strs[0]);
+        if (len <= 0) return "";
+        if (minLen > len) minLen = len;
+    }
+
+    i = 0;
+    while (i <= minLen) {
+        char c = first_str[i];
+        int j = 0;
+        for (j = 1; j < strsSize; j++) {
+            if (strs[j][i] == "\0" || strs[j][i] != c) {
+                comPre[i] = '\0';
+                return comPre;
+            }
+            comPre[i] = c;
+        }
+
+        i++;
+    }
+
+    return "";
+}
+
+int main()
+{
+    char* strs[] = {"leets", "leetcode", "leetc", "leeds"};
+    printf("longestCommonPrefix: %s\n", longestCommonPrefix(strs, sizeof(strs)/sizeof(char*)));
 }
