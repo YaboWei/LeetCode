2f7ce64027b7e3aa63a2ea7f1fe6ef6380a2415d
YaboWei <weiyabobeijing@gmail.com>
2017-08-20 00:09:20 +0800

    3Sum

--- 15.3Sum.c
+++ 15.3Sum.c
@@ -3,5 +3,36 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int** threeSum(int* nums, int numsSize, int* returnSize) {
-    
+    int i, j, k;
+    int** sumNums = malloc(10*sizeof(int));
+    *returnSize = 0;
+
+    for (i = 0; i < numsSize; i++) {
+        for (j = i+1; j < numsSize; j++) {
+            for (k = j+1; k < numsSize; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    printf("%d, %d, %d\n", nums[i], nums[j], nums[k]);
+                    int* list = malloc(3 * sizeof(int));
+                    list[0] = nums[i]; list[1] = nums[j]; list[2] = nums[k];
+                    sumNums[*returnSize] = list;
+                    *returnSize += 1;
+                }
+            }
+        }
+    }
+
+    return sumNums;
+}
+
+int main()
+{
+    int nums[] = {-1, 0, 1, 2, -1, -4};
+    int sumSize = 0;
+    int** sums = threeSum(nums, sizeof(nums)/sizeof(int), &sumSize);
+    printf("sumSize: %d\n", sumSize);
+
+    int i = 0;
+    for (i = 0; i < sumSize; i++) {
+        printf("%d, %d, %d\n", sums[i][0], sums[i][1], sums[i][2]);
+    }
 }
