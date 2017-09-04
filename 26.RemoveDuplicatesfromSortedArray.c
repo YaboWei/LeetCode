af2914d2efcfbbb4f42ded60e70529508655c5b3
YaboWei <weiyabobeijing@gmail.com>
2017-09-04 13:52:02 +0800

    Remove Duplicates from Sorted Array

--- 26.RemoveDuplicatesfromSortedArray.c
+++ 26.RemoveDuplicatesfromSortedArray.c
@@ -1,3 +1,26 @@
 int removeDuplicates(int* nums, int numsSize) {
-    
+    int i, sole_index = 0;
+    for (i = 1; i < numsSize;) {
+        if (nums[i] == nums[sole_index]) {
+            i++;
+            continue;
+        }
+
+        sole_index += 1;
+        nums[sole_index] = nums[i];
+        i++;
+    }
+
+    return sole_index + 1;
+}
+
+int main()
+{
+    int arr[] = {1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 7, 8, 8, 8, 9};
+    int len = removeDuplicates(arr, sizeof(arr)/sizeof(int));
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
