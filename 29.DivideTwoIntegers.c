8deb8db9d0241fff6ab375ef248716a00fa92c25
YaboWei <weiyabobeijing@gmail.com>
2017-09-04 15:39:10 +0800

    divide two integers

--- 29.DivideTwoIntegers.c
+++ 29.DivideTwoIntegers.c
@@ -1,3 +1,34 @@
 int divide(int dividend, int divisor) {
-    
+    int sign = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? 1 : -1;
+    if (dividend < 0) dividend = -dividend;
+    if (divisor  < 0) divisor  = -divisor;
+
+    int max_mod = 0;
+    while (dividend > divisor) {
+        divisor <<= 1;
+        max_mod++;
+    }
+
+    int res = 0;
+    divisor >>= max_mod;
+    max_mod--;
+
+    while (max_mod >= 0) {
+        printf("dividend: %d, divisor<<%d: %d\n", dividend, max_mod, divisor << max_mod);
+        if (dividend >= (divisor << max_mod)) {
+            dividend -= divisor << max_mod;
+            res += 1<<max_mod;
+        }
+        max_mod--;
+    }
+
+    return sign*res;
+}
+
+int main()
+{
+    int dividend = 87, divisor = 4;
+    int res = divide(dividend, divisor);
+    printf("%d / %d = %d\n", dividend, divisor, res);
+    return 0;
 }
