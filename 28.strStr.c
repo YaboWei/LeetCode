1fade37b9b07d3181cb6aeedcccd49c7f9a82687
YaboWei <weiyabobeijing@gmail.com>
2017-09-04 14:27:47 +0800

    implement strstr

--- 28.strStr.c
+++ 28.strStr.c
@@ -1,3 +1,25 @@
 int strStr(char* haystack, char* needle) {
-    
+    int i, len = strlen(haystack), need_len = strlen(needle);
+    if (len <= 0 || need_len <= 0) {
+        return -1;
+    }
+
+    for (i = 0; i < len - need_len; i++) {
+        if (strncmp(haystack+i, needle, need_len) == 0) {
+            return i;
+        }
+    }
+}
+
+int main()
+{
+    char* haystack = "abcdbcdbdb";
+    char* needle = "bd";
+
+    int start = strStr(haystack, needle);
+    if (start >= 0) {
+        printf("find needle '%s' at %d pos\n", needle, start);
+    } else {
+        printf("'%s' not found in '%s'\n", needle, haystack);
+    }
 }
